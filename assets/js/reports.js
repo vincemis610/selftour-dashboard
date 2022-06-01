e44b6379f531fd804e8fab0a811e458cd7c68a94
Vince Mis <vincemis610@gmail.com>
2022-06-01 00:33:43 -0500

    Se agreo switch on/off a la habilitacion de tours y fonto a las filas

--- assets/js/reports.js
+++ assets/js/reports.js
@@ -11,19 +11,26 @@ const storeData = async () => {
 // function que pobla la tabla de tours
 const poblarTablaTours = (data) => {
     console.log(data);
+
     let html = '';
     data.map( tour => { 
-        let status = (tour.status_tour === 0 || tour.cant_sitios === 0 || tour.images === 0) ? '<strong class="text-danger">Pendiente</strong>' : '<strong class="text-success">Aprovado</strong>';
-        console.log(status)
-        html = html + `<tr>
+        html = html + `<tr style="${(tour.status_tour === 1) ?'background-color:#eafaf1': 'background-color:#fbfcfc'}">
                 <td>${ (tour.title.length > 25) ? tour.title.substring(0, 25)+'...' : tour.title }</div></td>
-                <td>${ (tour.status_tour === 1) ? '<icon class="fa fa-check-circle enable"></icon>' : '<icon class="fa fa-ban disable"></icon>' }</td>
+                <td>${ (tour.status_tour === 1) ? `<label class="switch">
+                                                        <input type="checkbox" checked disabled>
+                                                        <span class="slider round"></span>
+                                                    </label>` 
+                                                : `<label class="switch">
+                                                        <input type="checkbox" disabled>
+                                                        <span class="slider round"></span>
+                                                    </label>`}
+                </td>
                 <td>${tour.tourmaker}</td>
                 <td>${(tour.cant_sitios === 0 ) ? '<icon class="fa fa-ban disable"></icon>' : tour.cant_sitios }</td>
                 <td>${(tour.images === 0) ? '<icon class="fa fa-exclamation-triangle disable"></icon>' : tour.images}</td>
                 <td>$ ${tour.price} ${tour.moneda}</td>
                 <td>${tour.ventas}</td>
-                <td>${ status }</td>
+                <td>${ tour.warnings.map( w => `<li style="${(w === 'Terminado') ? 'color:green' : 'color:red' }">${w}</li>`).join(' ') }</td>
             </tr>`
     });
 
@@ -34,6 +41,17 @@ const poblarUI = async () => {
     await storeData();
     let reportsTour = JSON.parse(localStorage.getItem('reportsTour'));
     poblarTablaTours(reportsTour.report);
+
+    const check = document.querySelector('.switch input');
+
+    check.addEventListener('change', (event) => {
+        console.log(event.target);
+    }, false);
+
 }
 
 poblarUI();
+
+
+
+
